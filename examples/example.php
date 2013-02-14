9851b4a21653e5e2b820901df7ec3c07131bd907
Jesus M. Castagnetto <jesus@upch.pe>
2013-02-14 17:20:19 -0500

    use composer autoload

--- examples/example.php
+++ examples/example.php
@@ -1,23 +1,19 @@
 <?php
-namespace PEAR2\Math;
-
-$current_path='/home/jesus/devel/jmc/github/PEAR2_Math_Distance';
-require_once $current_path.'/src/Math/Distance.php';
-require_once $current_path.'/src/Math/Distance/Exception.php';
-
-use PEAR2\Math\Distance;
-use PEAR2\Math\Distance\Exception;
+require "../vendor/autoload.php";
 
 $v1 = array(0,2,1);
 $v2 = array(1,4,5);
 
-$euclidean = Distance::euclidean($v1, $v2);
-$manhattan = Distance::manhattan($v1, $v2);
-$chebyshev = Distance::chebyshev($v1, $v2);
-$minkowsky = Distance::minkowsky($v1, $v2, 3);
+$euclidean = Math\Distance::euclidean($v1, $v2);
+echo "Euclidean distance: $euclidean\n";
+$manhattan = Math\Distance::manhattan($v1, $v2);
+echo "Manhattan distance: $manhattan\n";
+$chebyshev = Math\Distance::chebyshev($v1, $v2);
+echo "Chebyshev distance: $chebyshev\n";
+$minkowski = Math\Distance::minkowski($v2, $v1, 3);
+echo "Minkowski distance (order=3): $minkowski\n";
 
 $s1 = 'electric';
 $s2 = 'tectonic';
-$hamming = Distance::hamming($s1, $s2);
-
-?>
+$hamming = Math\Distance::hamming($s1, $s2);
+echo "Hamming distance: $hamming\n";
